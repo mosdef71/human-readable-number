34eae09bed3b93a7fbfb7ae6d220083d5ff2f5c8
mosdef71 <krag71@mail.ru>
2021-08-17 00:54:32 +0300

    fix: add trim method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 module.exports = function toReadable (number) {
   const array = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eightteen", "nineteen"]
-	const dozen = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-
+	const dozen = [ "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+	let dozenNum;
+	let str = String(number);
+	
 	if (number === 0) {
 		return array[0];
-	} else if (number < 20) {
+	
+  } else if (number < 20) {
 		return array[number];
+	
+  } else if (number < 100) {
+		
+		dozenNum = number % 10;
+		return `${dozen[str[0]]} ${array[dozenNum]}`.trim()
 	}
 
 
