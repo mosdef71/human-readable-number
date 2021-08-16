05e92563f12b7504f260649dbbb70ca384410fb6
mosdef71 <krag71@mail.ru>
2021-08-17 01:20:07 +0300

    feat: add dozen with last num < 20

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 module.exports = function toReadable (number) {
-  const array = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eightteen", "nineteen"]
-	const dozen = [ "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-	let dozenNum;
+  const array = [ "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eightteen", "nineteen"]
+	const dozen = [ "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 	let str = String(number);
+	let dozenNum = number % 10;
+	let firstHundredNum = number / 100;
+	let lastHundredNum = number % 100;
 	
 	if (number === 0) {
-		return array[0];
+		return "zero";
 	
-  } else if (number < 20) {
+	} else if (number < 20) {
 		return array[number];
-	
-  } else if (number < 100) {
-		
-		dozenNum = number % 10;
-		return `${dozen[str[0]]} ${array[dozenNum]}`.trim()
-	}
 
+	} else if (number < 100) {
+		return `${dozen[str[0]]} ${array[dozenNum]}`.trim()
+    
+	} else if (number > 100 && dozenNum < 20) {
+		return `${array[Math.floor(firstHundredNum)]} hundred ${array[lastHundredNum]}`.trim()
+	}
 
 
 
