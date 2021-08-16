5c24664f1d404ebbe166d186f42f9b625d06e559
mosdef71 <krag71@mail.ru>
2021-08-16 22:54:43 +0300

    init: start task hrn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+    let dict = {
+      1: "one",
+      2: "two",
+      3: "three",
+      4: "four",
+      5: "five",
+      6: "six",
+      7: "seven",
+      8: "eight",
+      9: "nine",
+      10: "ten",
+      11: "eleven",
+      12: "twelve",
+      13: "thirteen",
+      15: "fifteen",
+      20: "twenty",
+      30: "thirty",
+      40: "forty",
+      50: "fifty",
+    }
+    
+    return dict[number]
 }
